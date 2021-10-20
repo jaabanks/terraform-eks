0e1e9de85e6f36775392687a41e685f9a318a62b
Ubuntu <ubuntu@ip-172-31-47-219.ec2.internal>
2021-10-20 21:44:47 +0000

    updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "cluster_endpoint" {
   description = "Endpoint for EKS control plane."
   value       = module.eks.cluster_endpoint
 }
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = local.cluster_name
+}
