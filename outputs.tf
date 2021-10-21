8c77849347117d7b2ddb7454762ede9d25989e3e
Ubuntu <ubuntu@ip-172-31-47-219.ec2.internal>
2021-10-21 04:59:40 +0000

    updated outputs.tf again

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = local.cluster_name
 }
-
-output "ip-address" {
-  description = "Kubernetes Cluster IP_Adress"
-  value       = data.aws_eks_cluster.cluster.ip_address
-}
